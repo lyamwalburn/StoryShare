edb8ecf74107af80966849dced08a121a3a87d00
levids <lyam567@hotmail.co.uk>
2022-02-20 22:35:19 +0000

    finished google auth, todo fix issue with sessions. added functionaility to create stories and retreive users stories from db. protected routes and added nav

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const path = require('path')
 const express = require("express")
+const mongoose = require('mongoose')
 const dotenv = require("dotenv")
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
+//const MongoStore = require('connect-mongo')(session)          TODO fix crashes app
+
 
 
 //Load config
 dotenv.config({path: './config/config.env'})
 
+//Passport Config
+require('./config/passport')(passport)
+
 connectDB()
 
 const app = express()
+
+//Body parser
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
 
 //Logging
 if(process.env.NODE_ENV === 'development'){
@@ -23,11 +35,26 @@ app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+   // store: new MongoStore({ mongooseConnection: mongoose.connection})
+}))
+
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
